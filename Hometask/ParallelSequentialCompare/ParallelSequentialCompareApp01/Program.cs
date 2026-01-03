5c97a65db922f87db89af91f3d5492d4b0364b3c
gssoft <gs_soft@mail.ru>
2026-01-03 03:33:17 +0300

    ParallelSequentialCompare

---
+++ Hometask/ParallelSequentialCompare/ParallelSequentialCompareApp01/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    // Фиксированная общая папка
+    const string BasePath = @"./../../../../Files";
+
+    // Последовательное чтение трёх файлов
+    static void ReadFilesSequentially(params string[] fileNames)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        long totalSpaces = 0;
+
+        foreach (string fileName in fileNames)
+        {
+            string filePath = Path.Combine(BasePath, fileName);
+            string text = File.ReadAllText(filePath);
+            // totalSpaces += text.Count(c => char.IsWhiteSpace(c));
+            totalSpaces += text.Count(c => c == ' ');
+        }
+
+        sw.Stop();
+        Console.WriteLine($"Последовательное чтение трех файлов ({String.Join(", ", fileNames)}):\n" +
+                          $"Кол-во пробелов: {totalSpaces}\n" +
+                          $"Время выполнения: {sw.Elapsed.TotalMilliseconds:F2} ms");
+    }
+
+    // Параллельное чтение трёх файлов
+    static async Task ReadFilesParallelly(params string[] fileNames)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        long totalSpaces = 0;
+
+        List<Task<long>> tasks = new List<Task<long>>();
+
+        foreach (string fileName in fileNames)
+        {
+            string filePath = Path.Combine(BasePath, fileName);
+            tasks.Add(Task.Run(async () =>
+            {
+                string text = await File.ReadAllTextAsync(filePath);
+                // return (long)text.Count(c => char.IsWhiteSpace(c));
+                return (long)text.Count(c => c == ' ');
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+        totalSpaces = tasks.Sum(task => task.Result);
+
+        sw.Stop();
+        Console.WriteLine($"\nПараллельное чтение трех файлов ({String.Join(", ", fileNames)}):\n" +
+                         $"Кол-во пробелов: {totalSpaces}\n" +
+                         $"Время выполнения: {sw.Elapsed.TotalMilliseconds:F2} ms");
+    }
+
+    // Последовательное чтение папки
+    static void ReadFolderSequentially()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        long totalSpaces = 0;
+
+        foreach (string filePath in Directory.EnumerateFiles(BasePath))
+        {
+            string text = File.ReadAllText(filePath);
+            // totalSpaces += text.Count(c => char.IsWhiteSpace(c));
+            totalSpaces += text.Count(c => c == ' ');
+        }
+
+        sw.Stop();
+        Console.WriteLine("\nПоследовательное чтение папки:");
+        Console.WriteLine($"Кол-во пробелов: {totalSpaces}\n" +
+                         $"Время выполнения: {sw.Elapsed.TotalMilliseconds:F2} ms");
+    }
+
+    // Параллельное чтение папки
+    static async Task ReadFolderParallelly()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        long totalSpaces = 0;
+
+        IEnumerable<string> filePaths = Directory.EnumerateFiles(BasePath);
+        List<Task<long>> tasks = new List<Task<long>>();
+
+        foreach (string path in filePaths)
+        {
+            tasks.Add(Task.Run(async () =>
+            {
+                string text = await File.ReadAllTextAsync(path);
+                // return (long)text.Count(c => char.IsWhiteSpace(c));
+                return (long)text.Count(c => c == ' ');
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+        totalSpaces = tasks.Sum(task => task.Result);
+
+        sw.Stop();
+        Console.WriteLine("\nПараллельное чтение папки:");
+        Console.WriteLine($"Кол-во пробелов: {totalSpaces}\n" +
+                         $"Время выполнения: {sw.Elapsed.TotalMilliseconds:F2} ms");
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        // Список имен файлов для тестов
+        string[] fileNames = { "file1.txt", "file2.txt", "file3.txt" };
+
+        // Последовательное чтение трёх файлов
+        ReadFilesSequentially(fileNames);
+
+        // Параллельное чтение трёх файлов
+        await ReadFilesParallelly(fileNames);
+
+        // Последовательное чтение папки
+        ReadFolderSequentially();
+
+        // Параллельное чтение папки
+        await ReadFolderParallelly();
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+
---
+++ Hometask/ParallelSequentialCompare/ParallelSequentialCompareApp02/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Вариант 2. По лучше будет
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        string baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "Files");
+        string folderPath = Path.GetFullPath(baseDir);
+        Console.WriteLine($"Folder with Files is Here my Boy ...: {folderPath}");
+
+        // Кол-во прогонов
+        var runs = 5;
+        Console.WriteLine($"Кол-во прогонов: {runs}");
+
+        var files = Directory.GetFiles(folderPath, "*.txt", SearchOption.AllDirectories);
+
+        Console.WriteLine($"Найдено файлов: {files.Length}");
+
+        await RunBenchmarksAsync(files, runs);
+    }
+
+    // Запуск нескольких прогонов и усреднение
+    private static async Task RunBenchmarksAsync(string[] filePaths, int runs)
+    {
+        long totalSpacesSequential = 0;
+        long totalSpacesParallel = 0;
+
+        double[] seqTimes = new double[runs];
+        double[] parTimes = new double[runs];
+
+        for (int i = 0; i < runs; i++)
+        {
+            Console.WriteLine($"Прогон #{i + 1}");
+
+            // Последовательный
+            var swSeq = Stopwatch.StartNew();
+            totalSpacesSequential = await CountSpacesSequentialAsync(filePaths);
+            swSeq.Stop();
+            seqTimes[i] = swSeq.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine($"Последовательно: {totalSpacesSequential} пробелов, {swSeq.Elapsed.TotalMilliseconds:F2} мс");
+
+            // Параллельный (асинхронный по файлам)
+            var swPar = Stopwatch.StartNew();
+            totalSpacesParallel = await CountSpacesParallelAsync(filePaths);
+            swPar.Stop();
+            parTimes[i] = swPar.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine($"Параллельно:      {totalSpacesParallel} пробелов, {swPar.Elapsed.TotalMilliseconds:F2} мс");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Итог по всем прогонам:");
+
+        double avgSeq = seqTimes.Average();
+        double avgPar = parTimes.Average();
+
+        Console.WriteLine($"Среднее время последовательного варианта: {avgSeq:F2} мс");
+        Console.WriteLine($"Среднее время параллельного варианта:    {avgPar:F2} мс");
+
+        Console.WriteLine();
+        Console.WriteLine($"Проверка: последовательный = {totalSpacesSequential}, параллельный = {totalSpacesParallel}");
+    }
+
+    // Последовательный подсчёт
+    private static async Task<long> CountSpacesSequentialAsync(string[] filePaths)
+    {
+        long total = 0;
+
+        foreach (var path in filePaths)
+        {
+            string text = await File.ReadAllTextAsync(path);
+            total += text.LongCount(c => c == ' ');
+        }
+
+        return total;
+    }
+
+    // Параллельный подсчёт: одновременно читаем несколько файлов
+    private static async Task<long> CountSpacesParallelAsync(string[] filePaths)
+    {
+
+        var tasks = filePaths.Select(async path =>
+        {
+            string text = await File.ReadAllTextAsync(path);
+            return text.LongCount(c => c == ' ');
+        }).ToArray();
+
+        long[] results = await Task.WhenAll(tasks);
+        return results.Sum();
+    }
+}
